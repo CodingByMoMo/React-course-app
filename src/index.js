8ed4f6e2dfbd41a7fee2d487662ec6c06a5e5314
CodingByMoMo <bartek@momocodes.com>
2023-03-02 00:49:18 +0100

    Google Oauth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import express from "express";
-const app = express();
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { googleClientID, googleClientSecret } from "../config/keys.js";
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+const app = express();
+// Set up the passportJS library for google.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken',refreshToken);
+      console.log('profile',profile);
+    }
+  )
+);
+
+// Make call to google oauth and authenticate.
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+//Get a profile and access token.
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google")
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
