202aaef7244581797a7ac002af5f6bafeb7a4a72
ThrowMY <66215473+ThrowMY@users.noreply.github.com>
2023-03-05 22:05:04 +0100

    User model class

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema();
+
+const new_user_schema = () => {
+  const user_schema = new Schema({
+    google_ID: String,
+  });
+
+  mongoose.model("users", user_schema);
+};
+
+export { new_user_schema };
