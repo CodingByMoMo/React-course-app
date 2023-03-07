659e6e7cc7405d048a773ceb6f41be80a3c7f1b9
CodingByMoMo <bartek@momocodes.com>
2023-03-07 13:06:09 +0100

    Router fix #2

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import mongoose from "mongoose";
 import { new_user_schema } from "./models/user.js";
 import { mongoDB_base_URI } from "./config/keys.js";
 import { passport_config } from "./service/passport.js";
-import { auth_router } from "./routes/authRoutes.js ";
+import auth_router from "./routes/authRoutes.js ";
 
 mongoose.connect(mongoDB_base_URI);
 new_user_schema();
 passport_config();
 const app = express();
-app.use("/auth/google", auth_router);
-app.use("/auth/google/callback", auth_router);
-
+app.use("/", auth_router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
