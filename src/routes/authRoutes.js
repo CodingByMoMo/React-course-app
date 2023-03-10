f264626684d28c5f032fd03508e41fff7018b3cf
ThrowMY <66215473+ThrowMY@users.noreply.github.com>
2023-03-10 21:29:13 +0100

    Router Auth #1

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,24 +4,31 @@ import express from "express";
 const auth_router = express.Router();
 
 auth_router.use((req, res, next) => {
-  console.log('Action on: ', Date.now())
+  console.log("Action on: ", Date.now());
   next();
-})
+});
 
-auth_router.get("/", (req,res, next) => {
+auth_router.get("/", (req, res, next) => {
   res.send("Hello World");
 });
 
-auth_router.get("/auth/google", (req, res, next) => {
-  res.send("auth call");
-  // passport.authenticate("google", {
-  //   scope: ["profile", "email"]
-  // });
-});
+auth_router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+  (req, res) => {
+    res.status(200);
+  }
+);
 
-auth_router.get("/auth/google/callback", (req, res) => {
-  passport.authenticate("google");
-});
+auth_router.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    res.redirect("/~");
+  }
+);
 
 // // define the home page route
 // auth_router.get('/', (req, res) => {
