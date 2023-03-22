2756a974ee21ca82f5cd4da827957b01406242ed
CodingByMoMo <bartek@momocodes.com>
2023-03-22 01:51:05 +0100

    Add A React proxy to Express

---
+++ client/src/setupProxy.js
@@ -0,0 +1,15 @@
+import {
+  createProxyMiddleware,
+  Filter,
+  Options,
+  RequestHandler,
+} from "http-proxy-middleware";
+
+export function express_proxy(app) {
+  app.use(
+    ["/api", "/auth/google"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+    })
+  );
+};
