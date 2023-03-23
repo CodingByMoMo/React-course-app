056f53101d2338456250408e3f06b7950f7cda3d
CodingByMoMo <bartek@momocodes.com>
2023-03-23 01:18:00 +0100

    Fixed Middlewere Proxy

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const port = process.env.PORT || 5000;
+console.log(port);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
