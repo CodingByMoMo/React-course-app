40bf6294f71c99e9258d27538c3a277a1ebbd7fb
CodingByMoMo <bartek@momocodes.com>
2023-03-26 20:44:34 +0200

    Cleare React Setup, make a new setup with Redux.

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//  Import Libraries.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { legacy_createStore, applyMiddleware } from "redux";
+//  Import files and components.
+import App from "./components/App.js";
+import reportWebVitals from "./reportWebVitals.js";
+import reducers from "./reducers/index.js";
 
-const port = process.env.PORT || 5000;
-console.log(port);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//  Create store for Redux
+const store = legacy_createStore(reducers, {}, applyMiddleware());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
