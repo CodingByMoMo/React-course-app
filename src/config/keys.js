adb237d19298078efa152b9ab34c48e198cb1ee5
CodingByMoMo <bartek@momocodes.com>
2023-04-02 23:58:59 +0200

    Key stuff for Styripe

--- src/config/keys.js
+++ src/config/keys.js
@@ -3,6 +3,8 @@ import {
   google_client_secret_dev,
   mongoDB_base_URI_dev,
   cookie_key_dev,
+  stripe_public_key_dev,
+  stripe_secret_key_dev,
 } from "./dev.js";
 
 var google_client_ID = "";
@@ -10,6 +12,8 @@ var google_client_secret = "";
 var mongoDB_base_URI = "";
 var cookie_key = "";
 var callback_URL = "https://app.momocodes.com/auth/google/callback";
+var stripe_public_key = "";
+var stripe_secret_key = "";
 
 if (process.env.NODE_ENV === "production") {
   //Data Set for production.
@@ -17,12 +21,16 @@ if (process.env.NODE_ENV === "production") {
   google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
   mongoDB_base_URI = process.env.MONGODB_BASE_URI;
   cookie_key = process.env.COOKIE_KEY;
+  stripe_public_key = process.env.STRIPE_PUBLISHABLE_KEY;
+  stripe_secret_key = process.env.STRIPE_SECRET_KEY;
 } else {
   //Data set for developer envaiorment.
   google_client_ID = google_client_ID_dev;
   google_client_secret = google_client_secret_dev;
   mongoDB_base_URI = mongoDB_base_URI_dev;
   cookie_key = cookie_key_dev;
+  stripe_public_key = stripe_public_key_dev;
+  stripe_secret_key = stripe_secret_key_dev;
 }
 
 export {
