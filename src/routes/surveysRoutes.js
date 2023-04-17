4afcc18426d29e5561e2e6ebd89adcfae12566cb
CodingByMoMo <bartek@momocodes.com>
2023-04-17 23:59:11 +0200

    Add a new Surve route

--- src/routes/surveysRoutes.js
+++ src/routes/surveysRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
 import check_user_aut from "../middlewares/requireLogin.js";
 import check_user_credits from "../middlewares/creditsChecks.js";
+
+const Survey = mongoose.model("surveys");
 
 const surveys_router = express.Router();
 
@@ -8,8 +11,23 @@ surveys_router.get("/api/surveys", (req, res) => {
   res.send("surveys");
 });
 
-surveys_router.post("/api/surveys", check_user_aut, check_user_credits, (req, res) => {
-    res.send("new surveys");
-});
+surveys_router.post(
+  "/api/surveys",
+  check_user_aut,
+  check_user_credits,
+  (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      body,
+      subject,
+      recipients: recipients.split(",").map((email) => ({
+        email: email.trim(),
+      })),
+      _user: req.user.id,
+      date_sent: Date.now(),
+    });
+  }
+);
 
 export default surveys_router;
